72c173f5d2fd086e7b72f9018695458edbd9c294
Ali Mousa <amousa11@gmail.com>
2022-02-11 15:46:53 -0800

    update sdk to nodejs build

--- index.ts
+++ index.ts
@@ -1,7 +1,11 @@
-import {Account} from "@aleohq/sdk";
+import { Account } from "@aleohq/sdk";
 
 const main = () => {
-    console.log(Account);
+    let privateKey = "APrivateKey1zkpB5A1jLvjVHWjoCJkBqK2PmhoddZySASSQkY2nVdT4xhZ";
+    console.log("Loading account from private key: ", privateKey)
+    let account = Account.from_private_key(privateKey);
+    console.log("View Key: ", account.to_view_key());
+    console.log("Address: ", account.to_address());
 };
 
 main();
